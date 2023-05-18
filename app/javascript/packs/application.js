ce86a780694619539c777de22a45f8c066f0b771
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-05-18 14:51:59 +0000

    [finish]swiper_app-05182351

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
 import "script.js"
+import Swiper from 'swiper/swiper-bundle.js';
+import 'swiper/swiper-bundle.css';
+
 
 Rails.start()
 Turbolinks.start()
